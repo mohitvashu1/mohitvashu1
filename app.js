802be5f1348de9c1eebf78e8594a5c0b6edcecb5
mohitvashu1 <mohitchoubeybca@gmail.com>
2024-12-02 14:32:36 +0530

    Chapter-01

---
+++ app.js
@@ -0,0 +1,25 @@
+
+const parent=React.createElement(
+    "div",
+    {id:"parent"},
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1", {},"i am a h1 tag"),
+        React.createElement("h2", {},"i am a h2 tag"),
+    ]),
+    React.createElement("div",{id:"child"},[
+        React.createElement("h1", {},"i am a h1 tag"),
+        React.createElement("h2", {},"i am a h2 tag"),
+    ])
+    );
+
+
+
+
+
+
+
+
+const root= ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+
+                       
